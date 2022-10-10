92adaa2ec0f0eb93c2126e3f5aa3e8e0ca0ec396
Rodriguez <sergerodriguez.education@gmail.com>
2022-10-10 23:46:42 +0500

    feat: get quote
    Step 1
    Completed

--- scraper.py
+++ scraper.py
@@ -1,1 +1,10 @@
-# Web Scraper
+import requests, json
+
+res = requests.get(input('Input the URL:'))
+
+quote = res.json()
+
+if quote.get('content') is None:
+    print('Invalid quote resource!')
+else:
+    print(quote.get('content'))
