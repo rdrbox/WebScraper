93bf53e4415ca6e66d54e2d7a4490d41680a8dda
Rodriguez <sergerodriguez.education@gmail.com>
2022-10-18 11:55:11 +0500

    feat: step 4 completed

--- scraper.py
+++ scraper.py
@@ -1,24 +1,34 @@
 import requests
+import string, re
 
-url = input('Input the URL:')
+from bs4 import BeautifulSoup
+
+url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
 ok_status = 200
 
 r = requests.get(url)
 
-# if re.search('imdb.com/title', url):
-#     r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-#     if r.status_code == ok_status:
-#         soup = BeautifulSoup(r.content, 'html.parser')
-#         title = soup.find('h1').text
-#         description = soup.find('span', {'data-testid': 'plot-l'}).text
-#
-#         print({"title": title, "description": description})
-# else:
-#     print('Invalid movie page!')
-
 if r.status_code == ok_status:
-    with open('source.html', 'wb') as f:
-        f.write(r.content)
-    print('Content saved.')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    for item in soup.findAll('li', {'class': 'app-article-list-row__item'}):
+        article_type = item.find('span', {'data-test': 'article.type'}).text
+        if article_type.strip() == 'News':
+            url = item.find('a', {'data-track-action': 'view article'})['href']
+            req = requests.get(f'https://www.nature.com{url}')
+            if req.status_code == ok_status:
+                soup_news = BeautifulSoup(req.content, 'html.parser')
+                main_content = soup_news.find('div', {'class': 'c-article-body main-content'}).text
+                title = soup_news.find('h1').text.strip()
+
+                for i in title:
+                    if i in string.punctuation:
+                        title = title.replace(i, '')
+
+                new_title = re.sub(r'\s', '_', title)
+
+            with open(f"{new_title}.txt", "wb") as f:
+                f.write(main_content.encode('utf-8'))
+
+
 else:
     print('The URL returned', r.status_code)
